5e467ac47d5c38b629946e40113ab7908b0af5ab
alaxhenry <alaxhenry0121@gmail.com>
2022-12-15 12:27:22 +0900

    Initial Commit

---
+++ CoolBottomSheet/CoolBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBottomSheetApp.swift
+//  CoolBottomSheet
+//
+//  Created by Seungchul Ha on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct CoolBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
